841d97c5e6e463c4cf9a69a708ad54afe46c28a9
Bertrand49 <44713570+Bertrand49@users.noreply.github.com>
2018-11-03 05:26:39 +0200

    Add files via upload
    Js files

---
+++ js/toggle.js
@@ -0,0 +1,10 @@
+function toggle()
+{
+	document.getElementById("sidebar").classList.toggle('active');
+	document.getElementById("bottom").style.left="0";
+
+}
+function selected()
+{
+	document.getElementById("Home").style.border="1px 0px 0px 0px";
+}
